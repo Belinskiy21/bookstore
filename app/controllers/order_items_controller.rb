2413c22c2f2edeba15cc5fe4aa52df6a5546d6a2
Oleg <belinskiy21@gmail.com>
2017-09-22 14:16:04 +0300

    add order_items_quantity to helper and to cart icon

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -4,11 +4,14 @@ class OrderItemsController < ApplicationController
   def create
     @order_item = @order.order_items.find_by(book_id: params[:order_item][:book_id])
     if @order_item
-      @order_item.update_attribute(:quantity, @order_item.quantity + params[:order_item][:quantity].to_i, notice: 'Success!') 
+      @order_item.update_attribute(:quantity, @order_item.quantity + params[:order_item][:quantity].to_i)
+      redirect_to book_path(params[:order_item][:book_id]), notice: 'Book was added to you order!'
     else
       @order_item = @order.order_items.new(order_items_params)
       @order.user = current_user if user_signed_in?
       @order.save
+      redirect_to book_path(params[:order_item][:book_id]), notice: 'Book was added to you order!'
+
     end
     session[:order_id] = @order.id
     respond_to do |format|
--- app/helpers/order_items_helper.rb
+++ app/helpers/order_items_helper.rb
@@ -1,2 +1,5 @@
 module OrderItemsHelper
+  def order_items_quantity
+    current_order.order_items.collect { |item| item.quantity }.sum
+  end
 end

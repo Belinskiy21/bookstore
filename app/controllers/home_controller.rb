ab0982777eea7231d4e51b56afde476f42bca52e
Oleg <belinskiy21@gmail.com>
2017-10-11 18:13:24 +0300

    best_sellers in home_controller like popular_first done

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,7 @@
 class HomeController < ApplicationController
 
   def index
-    @books = Book.all.page(params[:page]).per(4)
+    @books = Book.popular_first.page(params[:page]).per(4)
     @new_books = Book.includes(:authors).last 3
     @order_item = OrderItem.new
   end

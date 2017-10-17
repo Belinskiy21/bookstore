5d76a6f97b45916e83ed6e11117c8c2bbce8fe2d
Oleg <belinskiy21@gmail.com>
2017-10-17 16:36:58 +0300

    remove lines from prod env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,10 +60,10 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
   # config.active_job.queue_name_prefix = "bookstore_#{Rails.env}"
-  config.action_mailer.perform_caching = false
-  config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { :host => 'belinskiy-bookstore.herokuapp.com' }
+  # config.action_mailer.perform_caching = false
+  # config.action_mailer.perform_deliveries = true
+  # config.action_mailer.raise_delivery_errors = true
+  # config.action_mailer.default_url_options = { :host => 'belinskiy-bookstore.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {

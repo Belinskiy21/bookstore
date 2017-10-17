a5fb996de610a116cd70079b16fe4950d5e610a4
Oleg <belinskiy21@gmail.com>
2017-10-17 16:29:04 +0300

    try with mailgun

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -69,6 +69,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: "smtp.gmail.com",
     port: '587',
+    domain: 'belinskiy-bookstore.herokuapp.com',
     authentication: :plain,
     enable_starttls_auto: true,
     user_name: ENV['USER_NAME'],

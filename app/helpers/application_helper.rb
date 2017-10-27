9164f1bd178ef46a765114422f8a00c484b5877e
Oleg <belinskiy21@gmail.com>
2017-10-27 12:09:27 +0300

    render tamplates with borders

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,4 +2,10 @@ module ApplicationHelper
   def authors_name_select(book)
     book.authors.pluck(:name).join(', ')
   end
+
+  def active_class(link_path)
+   return '' if request.GET.empty?
+   (link_path.include? request.GET.first.join('=')) ? 'active' : ''
+   # current_page?(link_path) ? 'active' : ''
+  end
 end
